69d6a1bb21721b3a4f095ce750893d28bfc693f2
Jarrett Ye <jarrett.ye@outlook.com>
2024-01-13 20:32:37 +0800

    use np.random.beta

--- ICI.r
+++ ICI.r
@@ -6,11 +6,14 @@ sample_size <- 10000
 number <- rbeta(sample_size, a, b)
 P <- sort(number)
 Y <- sapply(P, function(x) ifelse(runif(1) < x, 1, 0))
+# print(paste(P, collapse=", "))
+# print(paste(Y, collapse=", "))
 # P <- rep(mean(Y), sample_size)
 # P <- runif(sample_size)
-loess.calibrate <- loess(Y ~ P)
-P.calibrate <- predict(loess.calibrate, P)
-# P.calibrate <- lowess(P, Y, f=0.1, iter=0)$y
+# loess.calibrate <- loess(Y ~ P)
+# P.calibrate <- predict(loess.calibrate, P)
+P.calibrate <- lowess(Y ~ P, iter=0)$y
+# plot(density(P.calibrate))
 plot(P, P.calibrate, "l", xlim=c(0, 1), ylim=c(0, 1), xlab="P", ylab="P.calibrate", asp=1)
 abline(0, 1, col="red")
 ICI <- mean(abs(P.calibrate - P))
